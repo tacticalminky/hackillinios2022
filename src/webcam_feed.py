46885a4b75c7a4c9aa03aa255cb8da837b916ac5
Gautam Dayal <gautam.dayal@yahoo.com>
2022-02-26 00:55:03 -0600

    pushing code so elliot/jacob can work on their systems

--- src/webcam_feed.py
+++ src/webcam_feed.py
@@ -5,9 +5,14 @@ stream = cv2.VideoCapture(1)
 while True:
     ret, frame = stream.read()
     cv2.imshow('frame', frame)
+    """
+    1. get list of bounding box coordinates using detectMultiScale
+    2. Loop through list of coordinates and draw bounding box and label
+    3. Send data to servos/LEDs which will then actuate
+    """
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 stream.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
