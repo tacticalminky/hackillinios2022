c1cf1ecf85943540a5c4cb71b727d114931c5302
Gautam Dayal <gautam.dayal@yahoo.com>
2022-02-26 00:40:22 -0600

    added webcam stream

--- src/webcam_feed.py
+++ src/webcam_feed.py
@@ -1,2 +1,13 @@
 import cv2
 
+stream = cv2.VideoCapture(1)
+
+while True:
+    ret, frame = stream.read()
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+stream.release()
+cv2.destroyAllWindows()
