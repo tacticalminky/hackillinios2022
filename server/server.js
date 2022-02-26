746ff551d8b937dfc877e83cde58081cfa6d71c5
TacticalMinky <mink91094@gmail.com>
2022-02-26 04:24:22 -0600

    added basics for telegram notifications

--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
-/* server.js */
-
 const express = require('express');
 const path = require('path');
+const { Telegraf } = require('telegraf');
+const fs = require('fs');
+const envfile = require('envfile');
+require('dotenv').config();
 
 const app = new express();
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-app.get('/', function(request, response) {
-    response.sendFile(path.join(`${__dirname}/www/index.html`));
+bot.start((ctx) => {
+    let parsedFile = envfile.parse('.env');
+    parsedFile.BOT_TOKEN = process.env.BOT_TOKEN;
+    parsedFile.CHAT_ID = ctx.message.chat.id;
+    fs.writeFileSync('./.env', envfile.stringify(parsedFile));
+    ctx.reply('Successfully Connected');
+});
+
+bot.launch();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/www/index.html`));
 });
 
 app.use(express.static(path.join(__dirname, 'www')));
 
+app.post('/', (req, res) => {
+    console.log('Post recieved');
+    bot.telegram.sendMessage(process.env.CHAT_ID, 'Post recieved');
+
+});
+
 const port = 8000;
 app.listen(port, function() {
-    console.log(`Server started at http://localhost:${port}`)
+    console.log(`Server started at http://localhost:${port}`);
 });
+require('http').createServer(bot.webhookCallback('/secret-path')).listen(3000);
--- server/www/js/views/Main.js
+++ server/www/js/views/Main.js
@@ -3,12 +3,13 @@ import AbstactView from "./AbstactView.js";
 export default class extends AbstactView {
     constructor(params) {
         super(params);
-        this.setTitle("Settings");
     }
 
     async getHtml() {
         return `
-            <h1>Home Page!!</h1>
+            <form action="" method="POST">
+                <button type="submit">Test</button>
+            </form>
         `;
     }
 }
