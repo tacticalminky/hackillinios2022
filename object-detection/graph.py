0f3fdc995784d84f4cdfd3bc0881cfbe14932b4b
Gautam Dayal <gautam.dayal@yahoo.com>
2022-02-27 10:04:26 -0600

    barplot

--- object-detection/graph.py
+++ object-detection/graph.py
@@ -16,10 +16,26 @@ garbages = [0]
 for i in data["Garbage"]:
     garbages.append(i + garbages[-1])
 
-plt.plot(plastics, label="Plastic")
-plt.plot(papers, label="Paper")
-plt.plot(garbages, label="Garbage")
-plt.legend()
-plt.xlabel("Time")
-plt.ylabel("Units thrown")
+# plt.plot(plastics, label="Plastic")
+# plt.plot(papers, label="Paper")
+# plt.plot(garbages, label="Garbage")
+# plt.legend()
+# plt.xlabel("Time")
+# plt.ylabel("Units thrown")
+# plt.show()
+
+plt.style.use('ggplot')
+
+x = ['Plastics','Paper','Garbage']
+energy = [plastics[-1], papers[-1], garbages[-1]]
+
+x_pos = [i for i, _ in enumerate(x)]
+
+plt.bar(x_pos, energy, color='green')
+plt.xlabel("Category")
+plt.ylabel("Number")
+plt.title("Trash thrown by category")
+
+plt.xticks(x_pos, x)
+
 plt.show()
